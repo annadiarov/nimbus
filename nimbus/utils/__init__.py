90330c507bf1f518c61a662eaf54634a3695f8ea
annadiarov <annadiarov@gmail.com>
2024-05-26 22:51:56 +0200

    fix(utils): Fix imports in __init__

--- nimbus/utils/__init__.py
+++ nimbus/utils/__init__.py
@@ -1,3 +1,3 @@
-from logger_factory import LoggerFactory
+from .logger_factory import LoggerFactory
 from .ml_utils import *
-from cosine_warmup_scheduler import CosineWarmupScheduler
+from .cosine_warmup_scheduler import CosineWarmupScheduler
